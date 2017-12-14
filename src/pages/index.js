b33589daee98a8e37bde7a6b27dad08804335e0b
jacobworrel <jacobworrel@gmail.com>
2017-12-13 23:07:28 -0800

    finish video cover

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styles from './index.module.css';
+import cloudImg from './assets/Cloud_Surf.jpg';
+import cloudMp4 from './assets/Cloud_Surf.mp4';
+import cloudWebm from './assets/Cloud_Surf.webm';
+import logo from './assets/eightyminustwo-logo.png';
+
+const Logo = () => (
+  <Link to="/" className={styles.logo}>
+    <img src={logo} alt="Eighty Minus Two Logo" />
+  </Link>
+);
+
+const ShowcaseReel = () => (
+  <div className={styles.wrapper}>
+    <div className={styles.featured}>
+      <video
+        playsInline
+        autoPlay
+        muted
+        loop
+        poster={cloudImg}
+        className={styles.video}
+      >
+        <source src={cloudWebm} type="video/webm" />
+        <source src={cloudMp4} type="video/mp4" />
+      </video>
+    </div>
+  </div>
+);
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Logo />
+    <ShowcaseReel />
   </div>
 );
 
