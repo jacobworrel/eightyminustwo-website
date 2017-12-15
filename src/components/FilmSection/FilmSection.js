72d6f5c0639fda5bac2bc885400869a2bf3a6c05
jacobworrel <jacobworrel@gmail.com>
2017-12-15 10:08:14 -0800

    add hover state to films

--- src/components/FilmSection/FilmSection.js
+++ src/components/FilmSection/FilmSection.js
@@ -10,8 +10,18 @@ import dogLover4 from './assets/dogloverstill4.jpg';
 import singleDad1 from './assets/single-dad1.png';
 import whiteboywide from './assets/whiteboy-wide2.png';
 
-const Film = ({ title, src, layout }) => (
+const Film = ({ title, src, layout, director, cast }) => (
   <section className={`${layout} ${styles.column}`}>
+    <div className={styles.overlay}>
+      <div className={styles.director}>
+        <h4 className={styles.credit}>Directed by:</h4>
+        <p className={styles.name}>{director}</p>
+      </div>
+      <div className={styles.cast}>
+        <h4 className={styles.credit}>Starring:</h4>
+        <p className={styles.name}>{cast}</p>
+      </div>
+    </div>
     <img className={styles.img} src={src} alt={title} />
     <p className={styles.title}>{title}</p>
     <hr className={styles.hr} />
@@ -21,16 +31,36 @@ const Film = ({ title, src, layout }) => (
 const FilmSection = () => (
   <Element name="filmSection">
     <main id="films" className={styles.container}>
-      <Film title="Dog Lover" src={dogLover4} layout={styles.twoColumn} />
-      <Film title="Jesus Lady" src={jesusLadyStill3} layout={styles.twoColumn} />
+      <Film
+        title="Dog Lover"
+        director="Frederic Da"
+        cast="Sean Bolger"
+        src={dogLover4}
+        layout={styles.twoColumn} />
+      <Film
+        title="Jesus Lady"
+        director="Frederic Da"
+        cast="Melissa Knox, Dylan Taylor, Schuyler Jeffress"
+        src={jesusLadyStill3}
+        layout={styles.twoColumn} />
       <Film
         title="Fuckin White Boy"
+        director="Frederic Da"
+        cast="Ben Narich, Tracy Wilson, Chad Davis"
         src={whiteboywide}
         layout={styles.center}
       />
-      <Film title="Baby" src={baby1} layout={styles.twoColumn} />
+      <Film
+        title="Baby"
+        director="Roxane Mesquida"
+        cast="Roxane Mesquida, Frederic Da"
+        src={baby1}
+        layout={styles.twoColumn}
+      />
       <Film
         title="Single Dad"
+        director="Frederic Da"
+        cast="Sean Bolger"
         src={singleDad1}
         layout={styles.twoColumn}
       />
